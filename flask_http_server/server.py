2fa9e25f207ac9e821468fafb0448b5482867ad3
KKastaneda <ssnnp@mail.ru>
2019-12-18 00:40:06 +0300

    Now we want our server to be able to start "slave" upon receiving a corresponding request - the code proposed in server.py will be checked against the expectations.
    slave_start.yml Ansible playbook modified so that
    it prints the public IP address assigned to "slave",
    which can be further used to connect to the VM.

--- flask_http_server/server.py
+++ flask_http_server/server.py
@@ -1,10 +1,40 @@
+import os
+import subprocess
+import time
+
 from flask import Flask
 
+is_slave_running = False
+
 app = Flask(__name__)
+
+def terminate_slave():
+    subprocess.run(['ansible-playbook', '../vm_manager/slave_terminate.yml'])
+
+def start_slave():
+    start_time = time.time()
+    result = subprocess.run(['ansible-playbook', '../vm_manager/slave_start.yml'], capture_output=True)
+    if result.returncode != 0:
+        print('stdout:', result.stdout.decode('utf-8'))
+        print('stderr:', result.stderr.decode('utf-8'))
+        return
+    slave_ip = re.findall(r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b', result.stdout.decode('utf-8'))[0]
+    print('slave vm with public ip', slave_ip, 'has been started')
+    return slave_ip
+    
 
 @app.route('/')
 def index():
     return 'Nice to meet you'
+    
+@app.route('/start')
+def debug_start_slave():
+    return start_slave()
+    
+@app.route('/stop')
+def debug_stop_slave():
+    terminate_slave()
+    return 'Slave has been terminated'
     
 if __name__ == '__main__':
     app.run(debug='True', host='0.0.0.0', port=5000);
