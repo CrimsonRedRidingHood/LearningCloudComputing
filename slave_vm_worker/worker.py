574d2eaeec0df9314a5081b6b2093bc111cbaea9
KKastaneda <ssnnp@mail.ru>
2019-12-19 23:58:31 +0300

    Moving the master server to the port 80.
    Now there are 10 quotes in the list.

--- slave_vm_worker/worker.py
+++ slave_vm_worker/worker.py
@@ -1,11 +1,22 @@
 import random
 import socket
 
-quotes_list = ["Second thoughts are the reinforcements of the first thoughts you were trying to ignore (Steve Piccus)", "If I don't succeed, then let seed suck me. (Steve Piccus)"];
+quotes_list = [\
+"Everything popular is wrong (Oscrar Wilde)",\
+"Second thoughts are the reinforcements of the firsts thoughts you were trying to ignore. (Steve)",
+"No one can resolve your conflicts you battles in your heart, but you. (Steve)",\
+"It's just good business. (Lord Beckett)",\
+"Some are born great, others achieve greatness. (William Shakespeare)",\
+"You know, you can't be best at everything... hold on... no, it's just a limiting belief - you can be best at everything.",\
+"We never start our day with intention (Zan)",\
+"What most people actually like doing is crying complaining and dreaming.",\
+"Don't trust those who don't believe in you.",\
+"You are what you eat.",\
+]
 
-PORT = 5000
+port = 5000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('', PORT))
+server_socket.bind(('', port))
 server_socket.listen(1)
 connected_master, addr = server_socket.accept()
 while True:
